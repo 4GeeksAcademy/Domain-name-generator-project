89aaaad436c8787082f5bad970757e421c47be81
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-02-22 04:05:59 +0000

    improving

--- src/app.js
+++ src/app.js
@@ -7,41 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  
-    let domains = {
-      pronoun: ["the", "our", "my", "your", "their"],
-      adj: ["great", "big", "small", "fast", "slow"],
-      noun: ["jogger", "raccoon", "car", "tree", "house"],
-      extensions: [".com", ".net", ".org", ".io", ".co"]
-    };
-    let display = document.getElementById("bigList");
-    let domList = [];
 
+  let domains = {
+    pronoun: ["the", "our", "my", "your", "their"],
+    adj: ["great", "big", "small", "fast", "slow"],
+    noun: ["jogger", "raccoon", "car", "tree", "house"],
+    extensions: [".com", ".net", ".org", ".io", ".co"]
+  };
+  let display = document.getElementById("bigList");
+  let domList = [];
 
-    for (let i in domains.pronoun) {
-      for (let j in domains.adj) {
-        for (let t in domains.noun) {
-          for (let f in domains.extensions) {
-            domList.push(
-              domains.pronoun[i] +
-                domains.adj[j] +
-                domains.noun[t] +
-                domains.extensions[f]
-            );
-            display.innerText +=
-              domains.pronoun[i] +
+  for (let i in domains.pronoun) {
+    for (let j in domains.adj) {
+      for (let t in domains.noun) {
+        for (let f in domains.extensions) {
+          domList.push(
+            domains.pronoun[i] +
               domains.adj[j] +
               domains.noun[t] +
-              domains.extensions[f] +
-              "\n";
-          }
+              domains.extensions[f]
+          );
+          display.innerText +=
+            domains.pronoun[i] +
+            domains.adj[j] +
+            domains.noun[t] +
+            domains.extensions[f] +
+            "\n";
         }
       }
-    };
-    
-    console.groupCollapsed("All domains:");
-    domList.forEach(dom => console.log(dom));
-    console.groupEnd();
+    }
+  }
+
+  console.groupCollapsed("All domains:");
+  domList.forEach(dom => console.log(dom));
+  console.groupEnd();
 
   console.log("Hello Rigo from the console!");
 };
